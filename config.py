396ef0f23e922fc0361327cbc2d1167e5df02853
Your NameValeria-Leng <u3008920@connect.hku.hk>
2026-01-13 14:12:25 +0800

    the latest version

--- config.py
+++ config.py
@@ -6,13 +6,15 @@ _CN = CN()
 _CN.Nf=72
 _CN.Ns=14
 _CN.batch_size = 128
-_CN.epoch=50
-_CN.filename = './real_baseline'
-_CN.path_checkpoint = _CN.filename+'/ckpt_best_19.pth'  # 断点路径
+_CN.epoch=100
+_CN.filename = './Light_ChannelFromer/theory'
+_CN.path_checkpoint = _CN.filename+'/ckpt_best_ever39.pth'  # 断点路径
 # _CN.pkl_path = 'training_data/data.pkl'
 
 _CN.test_flag = False
 _CN.doppler = False
-_CN.complex = True
+_CN.complex = False
+_CN.no_CLN = False
+_CN.no_Cattention = True
 def get_cfg():
     return _CN.clone()
